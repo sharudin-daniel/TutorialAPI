65fca147971e53074aa81fcb018ac0a5ab89778b
dany <my@gmail.ru>
2022-03-22 12:08:02 +0300

    Initial commit

---
+++ src/test/java/com/example/tutorialapi/TutorialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
