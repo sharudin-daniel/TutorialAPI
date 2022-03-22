ab581d258e3280cc28a27d1778829fab9dc2e25b
dany <my@gmail.ru>
2022-03-22 12:26:13 +0300

    Lombock added

--- src/main/java/com/example/tutorialapi/controller/TutorialController.java
+++ src/main/java/com/example/tutorialapi/controller/TutorialController.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-//@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class TutorialController {
--- src/main/java/com/example/tutorialapi/model/Tutorial.java
+++ src/main/java/com/example/tutorialapi/model/Tutorial.java
@@ -1,13 +1,18 @@
 package com.example.tutorialapi.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "tutorials")
 public class Tutorial {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     @Column(name = "title")
     private String title;
@@ -26,37 +31,6 @@ public class Tutorial {
         this.published = published;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public boolean isPublished() {
-        return published;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setPublished(boolean published) {
-        this.published = published;
-    }
 
     @Override
     public String toString(){
